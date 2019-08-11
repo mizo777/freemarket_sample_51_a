fb32c3d18bab9485d8d6adf18fac73c47167cfaf
mizo777 <miniseven777@yahoo.co.jp>
2019-08-11 21:28:42 +0900

    Install haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' 
+      get 'done'
+    end
+  end
 end
