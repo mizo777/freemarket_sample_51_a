2e1fde78f8c738e78b3f1e070b212075bfeaec9e
mizo777 <miniseven777@yahoo.co.jp>
2019-08-11 14:35:27 +0900

    Create  form_split

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,18 @@
+class SignupController < ApplicationController
+  def step1
+    @user = User.new    
+  end
+  def step2
+    @user = User.new    
+  end
+  def step3
+    @user = User.new    
+  end
+  def step4
+    @user = User.new    
+  end
+  def done
+    @user = User.new    
+  end
+
+end
