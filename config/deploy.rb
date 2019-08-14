1c942e02793c7a8f464b6969ef4a63d9f701bb1f
Hroki <hiroki_12.22_goal@ace.ocn.ne.jp>
2019-08-14 12:17:38 +0900

    deploy.rbを修正しました

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "freemarket_sample_51_a"
-set :repo_url, "git@example.com:Hiroki/freemarket_sample_51_a.git"
+set :repo_url, "git@example.com:Hroki/freemarket_sample_51_a.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/MerukariKey.pem']  
+                  keys: ['~/.ssh/MerukariKey_pem.pem']  
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
