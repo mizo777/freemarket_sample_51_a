9d06f96fa2170b228eaafa0342f730135d8afd13
mizo777 <miniseven777@yahoo.co.jp>
2019-08-16 22:49:07 +0900

    Create signup_page

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,16 @@
 class SignupController < ApplicationController
+  def method_name
+    
+  end
+  def step1
+  end
+  def step2   
+  end
+  def step3   
+  end
+  def step4   
+  end
+  def done   
+  end
+
 end
