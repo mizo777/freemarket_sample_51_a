9ac22a033c820b605456fb8d17e9f744971360bf
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-08-16 21:01:01 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'markup_top_page#home'
-  #get 'markup_top_page', to: 'markup_top_page#home'
 end
