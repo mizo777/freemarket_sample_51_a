5a8e524d7e142a471bb75dd65518a322ca1885bf
mizo777 <miniseven777@yahoo.co.jp>
2019-08-23 14:06:02 +0900

    Update routes
    重複したルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # ログイン
   devise_for :users
   root 'markup_top_page#home'
   root 'markup_item_detail_page#home'  
@@ -8,16 +9,6 @@ Rails.application.routes.draw do
     collection do
       get 'detail'
       get 'purchase_confirmation'
-    end
-  end
-
-  resources :signup do
-    collection do
-      get 'step1'
-      get 'step2'
-      get 'step3'
-      get 'step4' 
-      get 'done'
     end
   end
   # マイページ
