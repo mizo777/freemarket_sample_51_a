9134bd49776ec502dfb9acf7f3487f562a455393
Hroki <hiroki_12.22_goal@ace.ocn.ne.jp>
2019-08-25 19:15:09 +0900

    環境変数を復活しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
   private
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
