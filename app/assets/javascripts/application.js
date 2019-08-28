482e593d6ae057a56b4c02dc3f8bc9cd961d4b47
mizo777 <miniseven777@yahoo.co.jp>
2019-08-28 19:52:50 +0900

    Update application.js
    jQueryが動かない問題を解消

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery.slick
 //= require rails-ujs
 //= require activestorage
+//= require turbolinks
 //= require jquery
-//= require jquery_ujs
+// require jquery_ujs
 //= require_tree .
 
 
