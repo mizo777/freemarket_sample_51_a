2bd5be8cb5cf4b57709866df907606dee32b3eee
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-08-29 15:27:57 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_013720) do
+ActiveRecord::Schema.define(version: 2019_08_29_003821) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "mail", null: false
