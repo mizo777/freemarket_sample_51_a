1f7ce5d54c1fbad5d8a230fff7210533415f0bd6
mizo777 <miniseven777@yahoo.co.jp>
2019-08-30 17:37:19 +0900

    Add  ActiveHash_Associations to products.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one :order
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :category
   belongs_to :product_image, dependent: :destroy
   belongs_to :brand
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  belongs_to_active_hash :region
 end
