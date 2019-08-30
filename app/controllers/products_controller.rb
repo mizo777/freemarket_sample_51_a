96f1e46678071f7451155333548912f18b812245
mizo777 <miniseven777@yahoo.co.jp>
2019-08-30 17:40:42 +0900

    Update Exhibit data_save from form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,30 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create!(product_params)
+  #   if @user.save
+  #     render 'index'
+  #   else
+  #     render 'new'
+  #  end    
   end
   
   def purchase_confirmation
   end
 end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :product_image_id, :detail, :price, :category_id, :brand_id, :state, :delivery_burden, :delivery_from, :delivery_way, :delivery_time, :size)
+  end
