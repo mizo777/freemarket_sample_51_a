f0f54141b53bcded9f76214b4c82e87f764faa14
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-09-04 17:21:13 +0900

    Update spec/factories files

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,26 +1,8 @@
 FactoryBot.define do
   factory :product do
-    # id {"1"}
-    # name {"シューズ"}
-    # price {2000}
-    # category_id {}
-    # user_id {}
-    # brand_id {}
-    # size {"24cm"}
-    # state {"新品、未使用"}
-    # delivery_burden {0}
-    # delivery_way {"らくらくメルカリ便"}
-    # delivery_from {"東京都"}
-    # delivery_time {"2~3日で発送"}
-    # detail {"新品です。"}
-    # likes_count {1}
-    # sold {"false"}
     id {"1"}
     name {"nakanaka様専用ルイヴィトン モノグラム キーケース 4連"}
     price {2500}
-    # category_id: {76}
-    # user_id: {2}
-    # brand_id: {3}
     size {""}
     state {"傷や汚れあり"}
     delivery_burden {0}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,18 +1,5 @@
 FactoryBot.define do
   factory :user do
-    # id {1}
-    # email {"jjj@i.softbank.jp"}
-    # password {"jjjaaaaa"}
-    # password_confirmation {"jjjaaaaa"}
-    # first_name {"拓未"}
-    # last_name {"大割"}
-    # first_kana_name {"タクミ"}
-    # last_kana_name {"オオワリ"}
-    # nickname {"takumi"}
-    # profile {"富山県出身です"}
-    # payment_way {"クレジットカード"}
-    # birthday {"1月2日生まれ"}
-    # confirmation_document {"証明書"}
     id {2}
     email {"aaaa@i.softbank.jp"}
     password {"aaaa0102"}
