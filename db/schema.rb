58b31a41f94a9c482eb197007b7a8855df01d347
mizo777 <miniseven777@yahoo.co.jp>
2019-09-07 15:27:20 +0900

    Add status_column to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_081810) do
+ActiveRecord::Schema.define(version: 2019_09_05_085828) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "mail", null: false
@@ -156,22 +156,23 @@ ActiveRecord::Schema.define(version: 2019_09_02_081810) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.bigint "category_id", null: false
+    t.string "name", default: "default", null: false
+    t.integer "price", default: 999, null: false
+    t.bigint "category_id", default: 35, null: false
     t.bigint "user_id"
     t.bigint "brand_id", default: 1
-    t.string "size", null: false
-    t.string "state", null: false
-    t.integer "delivery_burden", null: false
-    t.string "delivery_way", null: false
-    t.string "delivery_from", null: false
-    t.string "delivery_time", null: false
+    t.string "size", default: "Default", null: false
+    t.string "state", default: "999", null: false
+    t.integer "delivery_burden", default: 999, null: false
+    t.string "delivery_way", default: "Default", null: false
+    t.string "delivery_from", default: "Default", null: false
+    t.string "delivery_time", default: "Default", null: false
     t.text "detail"
     t.integer "likes_count"
     t.boolean "sold", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["delivery_burden"], name: "index_products_on_delivery_burden"
