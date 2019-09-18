5c8fae21e3f857fb9bf9e0f3f145a8487444dfe2
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-09-18 23:00:08 +0900

    Update payjp.js

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -5,19 +5,19 @@ $(function() {
     Payjp.setPublicKey("pk_test_8da00aba5ee999041f3a784e");
     form.find("input[type=submit]").prop("disabled", true);
     var card = {
-      card_number: $("#card_number").val(),
-      cvc: $("#cvc").val(),
-      expire_month: $("#expire_month").val(),
-      expire_year: $("#expire_year").val()
+      number: parseInt($("#number").val()),
+      cvc: parseInt($("#cvc").val()),
+      exp_month: parseInt($("#exp_month").val()),
+      exp_year: parseInt($("#exp_year").val())
     };
     console.log(card);
     Payjp.createToken(card, function(status, response) {
       if (status == 200) {
         console.log(status);
-        $(".card_number").removeAttr("name");
-        $(".cvc").removeAttr("name");
-        $(".expire_month").removeAttr("name");
-        $(".expire_year").removeAttr("name");
+        $("#number").removeAttr("name");
+        $("#cvc").removeAttr("name");
+        $("#exp_month").removeAttr("name");
+        $("#exp_year").removeAttr("name");
         form.append($('<input type="hidden" name="payjp-token">').val(response.id));
         form.get(0).submit();
         alert("登録が完了しました");
