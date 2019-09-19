93ff75e03533a8cf794cd7ff859c6cfafb371d3f
mizo777 <miniseven777@yahoo.co.jp>
2019-09-19 10:45:06 +0900

    Add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_023033) do
+ActiveRecord::Schema.define(version: 2019_09_19_013510) do
 
   create_table "addresses", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "mail", null: false
@@ -146,7 +146,7 @@ ActiveRecord::Schema.define(version: 2019_09_07_023033) do
   end
 
   create_table "product_images", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image"
+    t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
@@ -230,6 +230,8 @@ ActiveRecord::Schema.define(version: 2019_09_07_023033) do
     t.string "payment_way"
     t.string "birthday"
     t.text "confirmation_document"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["first_name"], name: "index_users_on_first_name"
     t.index ["last_name"], name: "index_users_on_last_name"
