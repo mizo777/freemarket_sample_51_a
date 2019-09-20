6c1121b250b35459903ae62c39798a20233856c6
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-09-20 13:03:16 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,16 +1,10 @@
 class CardsController < ApplicationController
   require "payjp"
-  before_action :set_card
-
-  def new
-    card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
-  end
 
   def pay
     Payjp.api_key = Settings.key[:payjp_secret_key]
     if  params['payjp-token'].blank?
-      redirect_to action: "new"
+      redirect_to card_create_mypage_index_path
     else
       customer = Payjp::Customer.create(
         description: 'test',
@@ -20,7 +14,7 @@ class CardsController < ApplicationController
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        redirect_to card_mypage_index_path(@card)
       else
         redirect_to action: "pay"
       end
@@ -36,38 +30,7 @@ class CardsController < ApplicationController
       customer.delete
       card.delete
     end
-    redirect_to action: "new"
+    redirect_to card_create_mypage_index_path(current_user.id)
   end
 
-  def show
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: "new"
-    else
-      Payjp.api_key = Settings.key[:payjp_secret_key]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-      # 登録しているカード会社のブランドアイコンを表示する
-      @card_brand = @default_card_information.brand      
-      case @card_brand
-      when "Visa"
-        @card_src = "visa.png"
-      when "JCB"
-        @card_src = "JCB.png"
-      when "MasterCard"
-        @card_src = "mastercard.png"
-      when "American Express"
-        @card_src = "americanExpress.png"
-      when "Diners Club"
-        @card_src = "dinersClub.png"
-      when "Discover"
-        @card_src = "discover.png"
-      end
-    end
-  end
-
-  private
-  def set_card
-    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
-  end
 end
