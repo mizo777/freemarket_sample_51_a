dfa1cda089211cc805d2f45ddb55b4e9f9fdc565
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-09-28 19:29:40 +0900

    Update product_search.js

--- app/assets/javascripts/product_search.js
+++ app/assets/javascripts/product_search.js
@@ -1,28 +1,14 @@
-$(document).on('turbolinks:load', function() {
-  $('#search_form').on('keyup', function() {
-    var input = $('#search_form').val();
-    $.ajax({
-      type: 'GET',
-      url: '/products/search',
-      data: {keyword: input},
-      dataType: 'json'
-    })
-    .done(function(products) {
-      $('#search_result').empty()
-      if (products.length !== 0 && input.length !== 0) {
-        products.forEach(function(product){
-          appendProduct(product);
-        });
-      }
-      else if (input.length == 0) {
-
-      }
-      else {
-      }
-    })
-    .fail(function(){
-      alert('商品検索に失敗しました');
-    })
-  });
-});
-
+//リロードしても商品検索フォームの値が消えず残る
+window.onload = function() {
+  var name = sessionStorage.getItem('name');
+  if(name !== null)$('#inputName').val(name);
+}
+window.onbeforeunload = function() {
+  sessionStorage.setItem("name", $('#inputName').val());
+}
+//商品検索フォームでEnterキーを押すと実行される
+function enter(){
+  if( window.event.keyCode == 13 ){
+    document.form1.submit();
+  }
+}
