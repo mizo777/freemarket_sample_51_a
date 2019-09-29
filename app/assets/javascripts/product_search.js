d38ad0f2aa9912173966dedd133febcd5645a552
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-09-29 16:32:43 +0900

    Update product_search.js

--- app/assets/javascripts/product_search.js
+++ app/assets/javascripts/product_search.js
@@ -1,8 +1,23 @@
-//リロードしても商品検索フォームの値が消えず残る
-window.onload = function() {
-  var name = sessionStorage.getItem('name');
-  if(name !== null)$('#inputName').val(name);
-}
+//トップページにおいて、リロードしても検索フォームの値が残る
+document.addEventListener("DOMContentLoaded", function(event) {
+  window.onload = function() {
+    var name = sessionStorage.getItem('name');
+    if(name !== null)$('#inputName').val(name);
+  }
+});
+//検索ページにおいて、リロードすると検索フォームの値がsubmitされ、検索結果が再描画される
+document.addEventListener("DOMContentLoaded", function(event) {
+  if (window.document.body.id === "product_search_page") {
+    if (window.performance) {
+      if (performance.navigation.type === 1) {
+        var name = sessionStorage.getItem('name');
+        if(name !== null)$('#inputName').val(name);
+        $('#search_form').submit();
+      }
+    }
+  }
+});
+//リロードした時、検索フォームの値をsessionStorageに保存する
 window.onbeforeunload = function() {
   sessionStorage.setItem("name", $('#inputName').val());
 }
@@ -12,3 +27,15 @@ function enter(){
     document.form1.submit();
   }
 }
+//検索フォームをクリックすると、枠線の色が変化する
+$(document).on('turbolinks:load', function() {
+  $('#inputName').on('click', function() {
+    $(this).addClass('touchForm');
+  });
+})
+//検索フォーム以外をクリックすると、検索フォームの枠線の色が元の色に戻る
+$(document).click(function(e){
+  if(!$(e.target).is('#inputName')) {
+    $('#inputName').removeClass('touchForm');
+  }
+});
