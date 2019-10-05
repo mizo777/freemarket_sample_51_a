582f8b3ba7f2b43f3b557ac2fac9820ab8928dbe
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-10-05 11:57:20 +0900

    Update like-btn js

--- app/assets/javascripts/button.js
+++ app/assets/javascripts/button.js
@@ -1,8 +1,22 @@
-$(function(){
-  $('.item-container__buttons--left__like-btn').on('click', function() {
+$(document).on('turbolinks:load', function() {
+  $('.like-btn').on('click', function() {
     $(this).css({'border':'1px solid #ea352d','background': 'none'});
-    $('.item-container__buttons--left__like-btn__text').css('color', '#ea352d');
-    $('.item-container__buttons--left__like-btn__count').css('color', '#ea352d');
+    $('#like-text').css('color', '#ea352d');
+    $('#like-count').css('color', '#ea352d');
     $('.heart-2').css('color', '#ea352d');
+    var count = $('#like-count').text();
+    count = parseInt(count);
+    $('#like-count').text(count+1);
+    console.log(count);
+  });
+  $('.button_to').on('submit', function() {
+    //$(this).prop("disabled", true);
+    $(this).css('pointer-events', 'none');
   });
 });
+// $(function(){
+//   $('.like-btn').on('submit', function() {
+//     $(this).prop("disabled", true);
+//     $('.like-btn').css('pointer-events', 'none');
+//   });
+// });
