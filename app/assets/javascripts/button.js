4d9b982410fee6508e4494960846523ee20d8c8c
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-10-06 08:29:07 +0900

    Update button.js

--- app/assets/javascripts/button.js
+++ app/assets/javascripts/button.js
@@ -25,31 +25,31 @@ $(document).on('turbolinks:load', function() {
   // }
   
   var countUpValue = 0;
-  var id = $('.item-container').attr('data-id');
+  var id = document.getElementByClassName("item-container").getAttribute("data-id");
+  //var $form = document.getElementByClassName("button_to");
 
   $('#like-btn-1').on('click', function() {
-    //var countUpValue = 0;
-    // var form = $(this);
-    // var url = $(this).attr('action');
     countUpValue++;
     console.log(countUpValue);
     //クリック回数が偶数回の時
     if ( (countUpValue) % 2 == 0 ) {
-      // var url = $('.button_to').attr('action');
-      // $('.button_to').attr({method: 'delete', action: url});
       $(this).css({'border':'1px solid #ea352d','background': 'none'});
       $('#like-text').css('color', '#ea352d');
       $('#like-count').css('color', '#ea352d');
       $('.heart-2').css('color', '#ea352d');
-      var like_count = $('#like-count').text();
-      like_count = parseInt(like_count);
-      $('#like-count').text(like_count+1);
 
-      $('.button_to').on('submit', function() {
-        //var id = $('.item-container').attr('data-id');
+      var count = $('#like-count').text();
+      count = parseInt(count);
+      $('#like-count').text(count+1);
+
+      $.when(
+        $('.button_to').on('submit', function() {
+        })
+      ).done(function() {
         var url = '/likes/' + id;
-        $('.button_to').removeattr('method action')
-        $('.button_to').attr({method: 'delete', action: url});
+        var $form = document.getElementsByClassName("button_to");
+        $form.action = url;
+        $form.method = "delete";
       });
     }
     //クリック回数が奇数回の時
@@ -58,63 +58,80 @@ $(document).on('turbolinks:load', function() {
       $('#like-text').css('color', '#333');
       $('#like-count').css('color', '#333');
       $('.heart-2').css('color', '#ccc');
-      // var product_id = $('.item-container').attr('data-id');
-      // document.form.action = "/likes";
-      // document.form.method = "post";
-      // var url = '/likes.' + id;
+
       var count = $('#like-count').text();
       count = parseInt(count);
       $('#like-count').text(count-1);
 
-      $('.button_to').on('submit', function() {
+      $.when(
+        $('.button_to').on('submit', function() {
+        })
+      ).done(function() {
         var url = '/likes.' + id;
-        $('.button_to').removeattr('method action')
-        $('.button_to').attr({method: 'post', action: url});
+        var $form = document.getElementsByClassName("button_to");
+        $form.action = url;
+        $form.method = "post";
+        //$('.button_to').removeAttr("method");
+        //$('.button_to').attr("method", "POST");
+        //$('.button_to').method = "post";
       });
     }
   });
+
   $('#like-btn-2').on('click', function() {
-    var countUpValue = 0;
     countUpValue++;
+    console.log(countUpValue);
     //クリック回数が偶数回の時
     if ( (countUpValue) % 2 == 0 ) {
       $(this).css({'border':'none','background': '#f5f5f5'});
       $('#like-text').css('color', '#333');
       $('#like-count').css('color', '#333');
       $('.heart-2').css('color', '#ccc');
+      var url = '/likes/' + id;
+      $('.button_to').removeAttr('method action')
+      $('.button_to').attr({method: 'delete', action: url});
 
       var count = $('#like-count').text();
       count = parseInt(count);
       $('#like-count').text(count-1);
 
-      $('.button_to').on('submit', function() {
+      $.when(
+        $('.button_to').on('submit', function() {
+        })
+      ).done(function() {
         var url = '/likes.' + id;
-        $('.button_to').removeattr('method action')
-        $('.button_to').attr({method: 'post', action: url});
+        var $form = document.getElementsByClassName("button_to");
+        $form.action = url;
+        $form.method = "post";
       });
     }
     //クリック回数が奇数回の時
     else {
-      var like_count = $('#like-count').text();
-      like_count = parseInt(like_count);
-      $('#like-count').text(like_count+1);
       $(this).css({'border':'1px solid #ea352d','background': 'none'});
       $('#like-text').css('color', '#ea352d');
       $('#like-count').css('color', '#ea352d');
       $('.heart-2').css('color', '#ea352d');
-      //console.log(like_count);
+      var url = '/likes.' + id;
+      $('.button_to').removeAttr('method action')
+      $('.button_to').attr({method: 'post', action: url});
 
-      $('.button_to').on('submit', function() {
-        //var id = $('.item-container').attr('data-id');
+      var count = $('#like-count').text();
+      count = parseInt(count);
+      $('#like-count').text(count+1);
+
+      $.when(
+        $('.button_to').on('submit', function() {
+        })
+      ).done(function() {
+        //var id = document.getElementByClassName("item-container").getAttribute("data-id");
         var url = '/likes/' + id;
-        $('.button_to').removeattr('method action')
-        $('.button_to').attr({method: 'delete', action: url});
+        var $form = document.getElementsByClassName("button_to");
+        $form.action = url;
+        $form.method = "delete";
+        //console.log($('.button_to').method);
+        // $form.removeAttribute("action");
+        // $form.setAttribute("action", url);
       });
     }
   });
-
-  //$('.button_to').on('submit', function() {
-    //$(this).prop("disabled", true);
-    //$(this).css('pointer-events', 'none');
-  //});
 });
