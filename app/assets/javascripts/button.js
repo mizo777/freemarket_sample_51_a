96603d939e0ca950ef7a4820fabf1f9550cdba8a
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-10-06 12:05:36 +0900

    Update button.js

--- app/assets/javascripts/button.js
+++ app/assets/javascripts/button.js
@@ -1,136 +1,114 @@
 $(document).on('turbolinks:load', function() {
   
-  // function appendLikeDestoryButton(product) {
-  //   var html = `<form class="button_to" method="delete" action="/likes/${product.id}" data-id="${product.id}">
-  //                 <button class="like-btn" id="like-btn-1" style="border: 1px solid #ea352d;background: none;" type="submit">
-  //                   <i class="fas fa-heart heart-2" style="color: #ea352d"></i>
-  //                   <span class="like-btn__text" id="like-text" style="color: #ea352d">いいね！</span>
-  //                   <span class="like-btn__count" id="like-count" style="color: #ea352d">${product.likes_count}</span>
-  //                 </button>
-  //                 <input type="hidden" name="authenticity_token" value="XeP3xV/sDNEEyBzrYkWCtWVunAt/ps9HsuoE7uHGkKa4oGhGjPUW5JWJA0+QFlAJN94UspwfOTMgb5RhroD4cw==">
-  //               </form>`;
-  //   $('.item-container__buttons--left').append(html);
-  // }
-
-  // function appendLikeCreateButton(product) {
-  //   var html = `<form class="button_to" method="post" action="/likes" data-id="${product.id}">
-  //                 <button class="like-btn" id="like-btn-1" style="border: 1px solid #ea352d;background: none;" type="submit">
-  //                   <i class="fas fa-heart heart-2" style="color: #ea352d"></i>
-  //                   <span class="like-btn__text" id="like-text" style="color: #ea352d">いいね！</span>
-  //                   <span class="like-btn__count" id="like-count" style="color: #ea352d">${product.likes_count}</span>
-  //                 </button>
-  //                 <input type="hidden" name="authenticity_token" value="XeP3xV/sDNEEyBzrYkWCtWVunAt/ps9HsuoE7uHGkKa4oGhGjPUW5JWJA0+QFlAJN94UspwfOTMgb5RhroD4cw==">
-  //               </form>`;
-  //   $('.item-container__buttons--left').append(html);
-  // }
-  
   var countUpValue = 0;
   var id = document.getElementByClassName("item-container").getAttribute("data-id");
-  //var $form = document.getElementByClassName("button_to");
 
+  //ユーザーによって、いいね！ボタンが既に押されているとき
   $('#like-btn-1').on('click', function() {
     countUpValue++;
-    console.log(countUpValue);
     //クリック回数が偶数回の時
     if ( (countUpValue) % 2 == 0 ) {
+      //ボタンを無効にする
+      $(this).prop("disabled", true);
+      //ボタンの色が変化する
       $(this).css({'border':'1px solid #ea352d','background': 'none'});
       $('#like-text').css('color', '#ea352d');
       $('#like-count').css('color', '#ea352d');
       $('.heart-2').css('color', '#ea352d');
-
+      //いいね！が+1される
       var count = $('#like-count').text();
       count = parseInt(count);
       $('#like-count').text(count+1);
-
+      //フォーム送信後、ボタンを有効にし、フォームのactionとmethodを変更する
       $.when(
         $('.button_to').on('submit', function() {
+          $(this).prop("disabled", false);
         })
       ).done(function() {
         var url = '/likes/' + id;
-        var $form = document.getElementsByClassName("button_to");
+        var $form = document.getElementsByClassName("button_to")[0];
         $form.action = url;
         $form.method = "delete";
       });
     }
     //クリック回数が奇数回の時
     else {
+      //ボタンを無効にする
+      $(this).prop("disabled", true);
+      //ボタンの色が変化する
       $(this).css({'border':'none','background': '#f5f5f5'});
       $('#like-text').css('color', '#333');
       $('#like-count').css('color', '#333');
       $('.heart-2').css('color', '#ccc');
-
+      //いいね！が-1される
       var count = $('#like-count').text();
       count = parseInt(count);
       $('#like-count').text(count-1);
-
+      //フォーム送信後、ボタンを有効にし、フォームのactionとmethodを変更する
       $.when(
         $('.button_to').on('submit', function() {
+          $(this).prop("disabled", false);
         })
       ).done(function() {
         var url = '/likes.' + id;
-        var $form = document.getElementsByClassName("button_to");
+        var $form = document.getElementsByClassName("button_to")[0];
         $form.action = url;
         $form.method = "post";
-        //$('.button_to').removeAttr("method");
-        //$('.button_to').attr("method", "POST");
-        //$('.button_to').method = "post";
       });
     }
   });
 
+  //ユーザーが初めていいね！ボタンを押すとき
   $('#like-btn-2').on('click', function() {
     countUpValue++;
-    console.log(countUpValue);
     //クリック回数が偶数回の時
     if ( (countUpValue) % 2 == 0 ) {
+      //ボタンを無効にする
+      $(this).prop("disabled", true);
+      //ボタンの色が変化する
       $(this).css({'border':'none','background': '#f5f5f5'});
       $('#like-text').css('color', '#333');
       $('#like-count').css('color', '#333');
       $('.heart-2').css('color', '#ccc');
-      var url = '/likes/' + id;
-      $('.button_to').removeAttr('method action')
-      $('.button_to').attr({method: 'delete', action: url});
-
+      //いいね！が-1される
       var count = $('#like-count').text();
       count = parseInt(count);
       $('#like-count').text(count-1);
-
+      //フォーム送信後、ボタンを有効にし、フォームのactionとmethodを変更する
       $.when(
         $('.button_to').on('submit', function() {
+          $(this).prop("disabled", false);
         })
       ).done(function() {
         var url = '/likes.' + id;
-        var $form = document.getElementsByClassName("button_to");
+        var $form = document.getElementsByClassName("button_to")[0];
         $form.action = url;
         $form.method = "post";
       });
     }
     //クリック回数が奇数回の時
     else {
+      //ボタンを無効にする
+      $(this).prop("disabled", true);
+      //ボタンの色が変化する
       $(this).css({'border':'1px solid #ea352d','background': 'none'});
       $('#like-text').css('color', '#ea352d');
       $('#like-count').css('color', '#ea352d');
       $('.heart-2').css('color', '#ea352d');
-      var url = '/likes.' + id;
-      $('.button_to').removeAttr('method action')
-      $('.button_to').attr({method: 'post', action: url});
-
+      //いいね！が+1される
       var count = $('#like-count').text();
       count = parseInt(count);
       $('#like-count').text(count+1);
-
+      //フォーム送信後、ボタンを有効にし、フォームのactionとmethodを変更する
       $.when(
         $('.button_to').on('submit', function() {
+          $(this).prop("disabled", false);
         })
       ).done(function() {
-        //var id = document.getElementByClassName("item-container").getAttribute("data-id");
         var url = '/likes/' + id;
-        var $form = document.getElementsByClassName("button_to");
+        var $form = document.getElementsByClassName("button_to")[0];
         $form.action = url;
         $form.method = "delete";
-        //console.log($('.button_to').method);
-        // $form.removeAttribute("action");
-        // $form.setAttribute("action", url);
       });
     }
   });
