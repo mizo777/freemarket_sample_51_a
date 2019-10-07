3d857dfebdd6af891b028ce3f3f9c80a7a2873ab
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-10-07 13:17:01 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,6 @@ class ProductsController < ApplicationController
     @random_products = Product.where(status: 0).order("RAND()").limit(2)
     @same_brand_products = Product.where(brand_id: @product.brand_id, status: 0).where.not(id: params[:id]).order("RAND()").limit(6)
     @exhibitor_related_products = Product.where(user_id: @product.user_id, status: 0).where.not(id: params[:id]).order("RAND()").limit(6)
-    @card = Card.where(user_id: current_user.id).first
     @like = Like.find_by(user_id: current_user.id, product_id: @product.id)
   end
 
