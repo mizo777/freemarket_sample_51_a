f12f00ea7da0fb805edc72f844794d96e18e7e93
mizo777 <miniseven777@yahoo.co.jp>
2019-10-08 16:13:01 +0900

    Add product_protect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :toggle_status, :pay, :buy, :destroy, :edit, :update]
   before_action :authenticate_user! , only: [:new, :buy] 
+  before_action :product_protect , only: [:edit, :destroy, :update] 
 
   def index
     @ladies_products = Product.ladies_products
@@ -44,16 +45,12 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    if @product.user_id == current_user.id
-      @parents = Category.limit(15)
-      @children = @product.category.parent.parent.children
-      @grandchildren = @product.category.parent.children
-      @brands = Brand.all
-      @image_count = @product.product_images.length
-      @product.product_images.build
-    else
-      redirect_to root_path, alert: '編集権限がありません'
-    end
+    @parents = Category.limit(15)
+    @children = @product.category.parent.parent.children
+    @grandchildren = @product.category.parent.children
+    @brands = Brand.all
+    @image_count = @product.product_images.length
+    @product.product_images.build
   end
 
   def update
@@ -139,5 +136,12 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(params[:id] || params[:product_id])
+  end
+  
+  def product_protect
+    if user_signed_in? && @product.user_id == current_user
+    else
+      redirect_to root_path, alert: '編集権限がありません'
+    end    
   end
 end
