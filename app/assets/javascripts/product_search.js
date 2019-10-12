bc1eda069c4bf142afd8e3a9706df158efd4a2c2
TAKUMI-OWARI <awsmk12@i.softbank.jp>
2019-10-12 19:30:38 +0900

    Update product_search.js

--- app/assets/javascripts/product_search.js
+++ app/assets/javascripts/product_search.js
@@ -1,10 +1,12 @@
-//トップページにおいて、リロードしても検索フォームの値が残る
-document.addEventListener("DOMContentLoaded", function(event) {
-  window.onload = function() {
-    var name = sessionStorage.getItem('name');
-    if(name !== null)$('#inputName').val(name);
-  }
-});
+//リロードしても検索フォームの値が残る(検索フォームの値はsessionStorageに保存される)
+window.onbeforeunload = function() {
+  sessionStorage.setItem("name", $('#inputName').val());
+}
+window.onload = function() {
+  var name = sessionStorage.getItem('name');
+  if(name !== null)$('#inputName').val(name);
+  if(name == 'undefined')$('#inputName').val("");
+}
 //検索ページにおいて、リロードすると検索フォームの値がsubmitされ、検索結果が再描画される
 document.addEventListener("DOMContentLoaded", function(event) {
   if (window.document.body.id === "product_search_page") {
@@ -17,10 +19,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   }
 });
-//リロードした時、検索フォームの値をsessionStorageに保存する
-window.onbeforeunload = function() {
-  sessionStorage.setItem("name", $('#inputName').val());
-}
 //商品検索フォームでEnterキーを押すと実行される
 function enter(){
   if( window.event.keyCode == 13 ){
