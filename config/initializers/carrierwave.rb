b8f281fbe4939d0290792b46e6f205c6f8cddf1c
mizo777 <miniseven777@yahoo.co.jp>
2019-10-14 16:28:51 +0900

    Change storage to file
    ファイルになら誰でも保存できるかの確認

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?  
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1' #例 'ap-northeast-1'
-    }
-    config.fog_directory  = 'mercari-free51'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-free51'
-  else
-    config.storage = :file
-  end
+  # if Rails.env.production?  
+  #   config.storage = :fog
+  #   config.fog_provider = 'fog/aws'
+  #   config.fog_credentials = {
+  #     provider: 'AWS',
+  #     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  #     region: 'ap-northeast-1' #例 'ap-northeast-1'
+  #   }
+  #   config.fog_directory  = 'mercari-free51'
+  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-free51'
+  # else
+  #   config.storage = :file
+  # end
+  config.storage = :file
 end
